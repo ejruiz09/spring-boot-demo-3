eebda0296515608eb01cbfed25cee21992d07bfb
Edward Ruiz <ejruiz09@ygmail.com>
2020-06-02 17:23:17 +0800

    Security login page

--- src/main/java/com/howtodoinjava3/app/controller/AsthmaForumPageController.java
+++ src/main/java/com/howtodoinjava3/app/controller/AsthmaForumPageController.java
@@ -1,5 +1,9 @@
 package com.howtodoinjava3.app.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +11,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AsthmaForumPageController {
 
-	@RequestMapping("/")
-	public String viewAsthmaForumPage(Model model) {
-		return "index";
-	}
+	/*
+	 * @RequestMapping("/") public String viewAsthmaForumPage(Model model) { return
+	 * "index"; }
+	 */
+	 
+	
+	
+	  //Login form
+	  
+	  @RequestMapping("/login.html") public String login() { return "login"; }
+	  
+	  //Login form with error
+	  
+	  @RequestMapping("/login-error.html") public String loginError(Model model) {
+	  model.addAttribute("loginError", true); return "login"; }
+	 
+	
+	/*
+	 * @RequestMapping("/securedPage") public String securedPage(Model
+	 * model, @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient
+	 * authorizedClient,
+	 * 
+	 * @AuthenticationPrincipal OAuth2User oauth2User) {
+	 * model.addAttribute("userName", oauth2User.getName());
+	 * model.addAttribute("clientName",
+	 * authorizedClient.getClientRegistration().getClientName());
+	 * model.addAttribute("userAttributes", oauth2User.getAttributes()); return
+	 * "securedPage"; }
+	 * 
+	 * @RequestMapping("/") public String index() { return "index2"; }
+	 */
 }
---
+++ src/main/java/com/howtodoinjava3/app/security/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.howtodoinjava3.app.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;  
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception{
+		http
+			.authorizeRequests()
+			.antMatchers("/public/**").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()
+			.loginPage("/login.html")
+			.failureUrl("/login-error.html")
+			.permitAll();
+			//.and()
+			//.httpBasic();
+			//.permitAll();
+			/*.antMatcher("/**")
+			.authorizeRequests()
+			.antMatchers("/", "/login**").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.oauth2Login();*/
+	}
+	
+	
+	  @Override protected void configure(AuthenticationManagerBuilder auth) throws
+	  Exception { auth.inMemoryAuthentication() .withUser("user")
+	  .password("{noop}pass") .roles("USER"); }
+	 
+}
